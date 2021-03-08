363e4db04b70bfe1320d901d46e0b682135d2ae8
Arshad Husain <arshad.husain1@gmail.com>
2021-03-08 02:48:23 +0000

    Implement open/close sensor change functionality
    This functionality detects if the door sensor is triggered and sends a
    request to the monitoring-server to notify of this change.

--- monitoring-server/monitoring.py
+++ monitoring-server/monitoring.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify, Response
 
 
+garage_activity = {}
+
 app = Flask(__name__)
 
 @app.route('/openalert', methods=['GET'])
@@ -13,14 +15,54 @@ def send_alert():
 		#########################################################
 		#Send Amazon SNS text message to phone for notifications
 		#########################################################
-		return 'OK'
+		return Response('OK', status=200, mimetype='text/html')
 
 	except AssertionError as a:
 		return Response(f'Unable to process. Reason: {a}', status=400, mimetype='text/html')
 	except Exception as e:
 		return Response(f'Unable to process. Reason: {e}', status=500, mimetype='text/html')
 
+@app.route('/sensorchange', methods=['GET'])
+def door_sensor_change():
+	door_status = request.args.get('door_status')
+	date = request.args.get('date')
+	time = request.args.get('door_status')
 
+	try:
+		if not door_status or not date or not time:
+			params_missing = "\n"
+			if not door_status:
+				params_missing+=str("door_status\n")
+			if not date:
+				params_missing+=str("date\n")
+			if not time:
+				params_missing+=str("time\n")
+
+			#TODO: Return missing param arguments as json
+			raise AssertionError(f"The following params are missing: {params_missing}")
+
+
+		garage_activity = {
+		    'door_status': door_status,
+		    'date': date,
+		    'time': time
+		}
+
+
+		#########################################################
+		#Send POST in JSON form to the logging server, e.g.
+		#{
+		#	door_status: OPEN
+		#	date: 05/29/2021
+		#	time: 17:41:00
+		#}
+		#########################################################
+		return Response('OK', status=200, mimetype='text/html')
+
+	except AssertionError as a:
+		return Response(f'Unable to process. Reason: {a}', status=400, mimetype='text/html')
+	except Exception as e:
+		return Response(f'Unable to process. Reason: {e}', status=500, mimetype='text/html')
 
 if __name__ == "__main__":
 		app.run(debug=True, host='0.0.0.0', port=5001)
--- sensor-server/sensor.py
+++ sensor-server/sensor.py
@@ -12,36 +12,62 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def door_open():
 	print("Door is open")
 	time_opened = datetime.now()
-	#########################################################
-	#POST open status date/time to monitoring server
-	#########################################################
+	print(time_opened)
+	try:
+		##################################################################################################################
+		#POST open status date/time to monitoring server
+		sensorchange = requests.get('http://localhost:5001/sensorchange',
+									params={'door_status': "opened",
+											'date': time_opened.strftime("%x"),
+											'time': time_opened.strftime("%X")})
+		##################################################################################################################
+	except Exception as e:
+		###########################################################
+		# TODO: LOG ERROR AND COME UP WITH WAY TO ALERT OF FAILURE
+		###########################################################
+		print(f"CANNOT SEND DOOR OPENED STATUS TO MONITORING SERVER. {e}")
 	while(1):
-		if((int((datetime.now()-time_opened).total_seconds()) % 600) == 0 and (int((datetime.now()-time_opened).total_seconds()) != 0)):
+		time_now = datetime.now()
+		if((int((time_now-time_opened).total_seconds()) % 600) == 0 and
+			(int((time_now-time_opened).total_seconds()) != 0)):
 			try:
-				#########################################################
-				#Send POST to monitoring server /openalert endpoint if door is open for 10 minutes
-				r = requests.get('http://localhost:5001/openalert', params={'minutes': int((datetime.now()-time_opened).total_seconds() / 60)})
-				#########################################################
+				##################################################################################################################
+				#Send GET to monitoring server /openalert endpoint if door is open for 10 minutes
+				openalert = requests.get('http://localhost:5001/openalert',
+										 params={'minutes': int((time_now-time_opened).total_seconds() / 60)})
+				##################################################################################################################
 			except Exception as e:
 				###########################################################
 				# TODO: LOG ERROR AND COME UP WITH WAY TO ALERT OF FAILURE
 				###########################################################
 				print(f"CANNOT SEND ALERT TO MONITORING SERVER. {e}")
-			print(f"WARNING: Garage has been open for {int((datetime.now()-time_opened).total_seconds() / 60)} minutes")
+			print(f"WARNING: Garage has been open for {int((time_now-time_opened).total_seconds() / 60)} minutes")
 			sleep(1)
 		if(GPIO.input(16) == GPIO.HIGH):
-			time_closed = datetime.now()
-			print(f"Door was opened for {(time_closed-time_opened).total_seconds()}")
+			print(f"Door was opened for {(time_now-time_opened).total_seconds()}")
 			return
 
 
 def door_closed():
 	print("Door is closed")
+	time_closed = datetime.now()
+	print(time_closed)
+	try:
+		#########################################################
+		#POST close status date/time to monitoring server
+		sensorchange = requests.get('http://localhost:5001/sensorchange',
+								params={'door_status': "closed",
+										'date': time_closed.strftime("%x"),
+										'time': time_closed.strftime("%X")})
+		#########################################################
+	except Exception as e:
+		###########################################################
+		# TODO: LOG ERROR AND COME UP WITH WAY TO ALERT OF FAILURE
+		###########################################################
+		print(f"CANNOT SEND DOOR CLOSED STATUS TO MONITORING SERVER. {e}")
 	while(1):
-		#########################################################
-		#POST close status date/time to monitoring server]
-		#########################################################
 		if(GPIO.input(16) == GPIO.LOW):
+			print(f"Door was opened for {(datetime.now()-time_closed).total_seconds()}")
 			return
 
 def main():
@@ -53,6 +79,8 @@ def main():
 				door_closed()
 	except KeyboardInterrupt:
 		print("Ctrl-C Recieved. Closing sensor.py")
+	except Exception as e:
+		print(f"Sensor service failed: {e}")
 
 
 
