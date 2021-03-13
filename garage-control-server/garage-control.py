beb957ab9e00b22d53f1acbce477777cf88faca4
Arshad Husain <arshad.husain1@gmail.com>
2021-03-13 22:23:43 +0000

    SG-32: add authentication for garage door toggle
    This allows access to the frontend web app but only allows use of the
    garage door button if the correct username and password is given. A
    config.json also needs to be included but will not be added to the repo
    for security purposes.

--- garage-control-server/garage-control.py
+++ garage-control-server/garage-control.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify, Response
 import json
 import time
 import RPi.GPIO as GPIO
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -10,8 +12,21 @@ GPIO.setup(7, GPIO.OUT)
 GPIO.output(7, GPIO.HIGH)
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    f = open('config.json')
+    data = json.load(f)
+    users = data["users"]
+    f.close()
+
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 @app.route('/openclose', methods=['POST'])
+@auth.login_required
 def Garage():
 	try:
 		GPIO.output(7, GPIO.LOW)
