798ad449d0f51cf9309d5bf7b4a002c9d882cdbe
Arshad Husain <arshad.husain1@gmail.com>
2021-03-31 00:50:08 +0100

    SG-52: Removed redundant redis declaration
    This app.config declaration has no use for the function of redis for
    sending notifications.

--- monitoring-server/monitoring.py
+++ monitoring-server/monitoring.py
@@ -165,6 +165,3 @@ def stream():
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5001, ssl_context=('cert.pem', 'key.pem'))
-
-	#Check if this is necessary
-	app.config["REDIS_URL"] = "redis://127.0.0.1:6379/0"
